dcdab55be142715622e6f405cfa2945e453ec13d
David <dfandinodovalo@gmail.com>
2024-09-04 20:16:35 +0200

    Initial commit: Digital Signature API project setup

---
+++ src/main/java/com/davidfandino/digital_signature_api/DigitalSignatureApiApplication.java
@@ -0,0 +1,13 @@
+package com.davidfandino.digital_signature_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalSignatureApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalSignatureApiApplication.class, args);
+	}
+
+}
