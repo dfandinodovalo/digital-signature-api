5e658096d8e4809849b083337b3c77d3c297f9a4
David <dfandinodovalo@gmail.com>
2024-09-08 20:11:04 +0200

    Add automatic userUUID generation

--- src/main/java/com/davidfandino/digital_signature_api/model/User.java
+++ src/main/java/com/davidfandino/digital_signature_api/model/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -15,6 +16,12 @@ import java.util.UUID;
 public @Data class User {
 
     @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(updatable = false, nullable = false)
     private UUID userUUID;
     private String firstName;
     private String lastName;
